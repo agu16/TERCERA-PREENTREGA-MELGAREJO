0a9d32427254f655da0caa3157226219ac17c198
Agu16 <agustinmelgarejo16@gmail.com>
2023-04-11 23:38:40 -0300

    actualizacion formulario

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
 class SeleccionLibroFormulario(forms.Form ):
-    nombre = forms.forms.CharField( max_length= 40)
-    autor = forms.forms.forms.CharField( max_length=20)
+    nombre = forms.CharField( max_length= 40)
+    genero = forms.CharField( max_length= 40)
+    autor = forms.CharField( max_length= 40)
+    editorial = forms.CharField( max_length= 40)
     
     
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Libros
 from django.shortcuts import redirect
 from .models import Libros
+from .forms import SeleccionLibroFormulario
 
 # Create your views here.
 
@@ -9,20 +10,14 @@ def plantilla_hija(request):
     return render (request , "plantilla_hija.html", {})
 
 def crear_libro(request):
-    if request.method == "POST":
-        objeto = Libros(
-        nombre = request.POST["nombre"],
-        genero = request.POST["genero"],
-        autor = request.POST["autor"],
-        editorial =  request.POST["editorial"],
-        )
-        objeto.save() 
-        return redirect ('detalle_objeto', pk=objeto.pk)
-    else:
-        return render(request, 'crear_libro.html')
+     if request.method == "POST":
+        form = SeleccionLibroFormulario(request.POST)
+        if form.is_valid():
+            objeto = form.save()
+            return redirect('detalle_objeto', pk=objeto.pk)
+     else:
+        form = SeleccionLibroFormulario()
+     return render(request, 'crear_libro.html', {'form': form})
     
-def buscar_libro(request):
-    if request.method == "POST":
-         valor_buscado = request.POST['valor_buscado']
-         objeto = get_object_or_404 ( buscar_libro, campo_busqueda=valor_buscado) 
+
     
