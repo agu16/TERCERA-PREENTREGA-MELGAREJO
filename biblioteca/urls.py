ab8877bb3f5cd94a776dfd61cda3b677e77ffec6
Agu16 <agustinmelgarejo16@gmail.com>
2023-04-11 23:57:21 -0300

    Ultima actualizacion para correccion

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from biblioteca import views
 from .views import crear_libro
+from .views import buscar_libro
 
 app_name = "biblioteca"
 
 urlpatterns = [
     path("plantilla_hija/" , views.plantilla_hija),
     path ("crear_libro/" , crear_libro , name = "crear_libro"),
+    path ("buscar_libro/" , buscar_libro , name = "buscar_libro" ),
     ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -3,6 +3,7 @@ from .models import Libros
 from django.shortcuts import redirect
 from .models import Libros
 from .forms import SeleccionLibroFormulario
+from .models import Libros
 
 # Create your views here.
 
@@ -10,14 +11,24 @@ def plantilla_hija(request):
     return render (request , "plantilla_hija.html", {})
 
 def crear_libro(request):
-     if request.method == "POST":
+    if request.method == "POST":
         form = SeleccionLibroFormulario(request.POST)
         if form.is_valid():
-            objeto = form.save()
-            return redirect('detalle_objeto', pk=objeto.pk)
-     else:
+            libro = form.save()  # Guardar el formulario en la base de datos
+            return redirect('detalle_objeto', pk=libro.pk)
+    else:
         form = SeleccionLibroFormulario()
-     return render(request, 'crear_libro.html', {'form': form})
+    return render(request, 'crear_libro.html', {'form': form})
+
+
+
+def buscar_libro(request):
+     if request.method == 'POST':
+         valor_buscado = request.POST['valor_buscado']
+         Libros , campo_busqueda = valor_buscado
+         return render(request, 'detalle_objeto.html', {'objeto': objeto})
+     else:
+          return render(request, 'formulario_buscar_libro.html')
     
 
     
